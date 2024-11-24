4c536d2e99d39b583dac2ed2f807adcd00e66fc4
lenah23 <lenulstep@gmail.com>
2024-11-24 16:08:47 +0400

    unit testing example

--- src/tests/CustomInput.test.tsx
+++ src/tests/CustomInput.test.tsx
@@ -3,7 +3,6 @@ import { CustomInput } from '../components';
 
 describe('CustomInput Component', () => {
   const mockOnChange = jest.fn();
-  const mockHandleKeyPress = jest.fn();
 
   it('renders with the correct placeholder', () => {
     render(
@@ -27,23 +26,5 @@ describe('CustomInput Component', () => {
     const input = screen.getByPlaceholderText('Enter text');
     fireEvent.change(input, { target: { value: 'New value' } });
     expect(mockOnChange).toHaveBeenCalledTimes(1);
-  });
-
-  it('calls handleKeyPress handler when a key is pressed', () => {
-    render(
-      <CustomInput
-        onChange={mockOnChange}
-        placeholder='Enter text'
-        value=''
-      />
-    );
-    const input = screen.getByPlaceholderText('Enter text');
-
-    input.focus();
-    fireEvent.keyPress(input, { key: 'Enter', code: 'Enter', charCode: 13 });
-
-    console.log(mockHandleKeyPress.mock.calls); 
-
-    expect(mockHandleKeyPress).toHaveBeenCalledTimes(1);
   });
 });
